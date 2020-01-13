cefd311316335ecf6c3bd953793a1c531d1308f9
nefarious_onion <kokkonen.anne@outlook.com>
2020-01-13 13:23:40 +0200

    delete test code

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,4 @@ var b = 2;
 
 var c = a + b;
 
-//console.error(c);
-
-if (a < b ) {
-    //do the following of a is lesser than b
-    console.log("a is lesser than b");
-    console.log(a + " is lesser than " + b);
-} else {
-    //do the following otherwise
-    console.log("a is not greater than b")
-}
-
-// jksjdkoj command + shift + 6
-
-if (a === 1) {
-    console.log("a is exactly 1")
 }
