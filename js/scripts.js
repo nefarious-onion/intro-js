af0a732fa82b128c85842757474912c628418135
nefarious_onion <kokkonen.anne@outlook.com>
2020-01-13 13:19:43 +0200

    add some test javascript

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+console.log("Hello again");
+
+var a = 1;
+var b = 2;
+
+var c = a + b;
+
+//console.error(c);
+
+if (a < b ) {
+    //do the following of a is lesser than b
+    console.log("a is lesser than b");
+    console.log(a + " is lesser than " + b);
+} else {
+    //do the following otherwise
+    console.log("a is not greater than b")
+}
+
+// jksjdkoj command + shift + 6
+
+if (a === 1) {
+    console.log("a is exactly 1")
+}
